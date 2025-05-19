a9ade8c5c019493faeabee6e2e4592165c2cd8fa
James Geiger <jgeiger2@me.com>
2025-05-19 15:37:48 -0400

    Remove /admin redirect for best practice dashboard auth handling

--- next.config.mjs
+++ next.config.mjs
@@ -47,15 +47,6 @@ const nextConfig = {
       },
     ];
   },
-  async redirects() {
-    return [
-      {
-        source: "/admin",
-        destination: "/admin/login",
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
